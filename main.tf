ba62325feec35a84ea71ec7d6401eb403176de76
Silpashree Acharya <silpashree@bootlabstech.com>
2023-08-03 12:39:35 +0530

    added lifecycle block

--- main.tf
+++ main.tf
@@ -4,5 +4,8 @@ resource "aws_sqs_queue" "terraform_queue" {
   delay_seconds             = var.delay_seconds
   max_message_size          = var.max_message_size
   message_retention_seconds = var.message_retention_seconds
-  receive_wait_time_seconds =var.receive_wait_time_seconds 
+  receive_wait_time_seconds =var.receive_wait_time_seconds
+    lifecycle {
+    ignore_changes = [tags]
+  } 
 }
