e4c84aa2dfad8303e0d758b66bfa23b36c2b5b4f
Silpashree Acharya <silpashree@bootlabstech.com>
2023-12-29 11:20:11 +0530

    added FIFO and CMEK

--- main.tf
+++ main.tf
@@ -1,11 +1,18 @@
 resource "aws_sqs_queue" "terraform_queue" {
-  count                     = var.no_of_queues  
-  name                      = var.queue_name[count.index]
-  delay_seconds             = var.delay_seconds
-  max_message_size          = var.max_message_size
-  message_retention_seconds = var.message_retention_seconds
-  receive_wait_time_seconds =var.receive_wait_time_seconds
-    lifecycle {
+  count                               = var.no_of_queues  
+  name                                = var.queue_name[count.index]
+  visibility_timeout_seconds          = var.visibility_timeout_seconds
+  delay_seconds                       = var.delay_seconds
+  max_message_size                    = var.max_message_size
+  message_retention_seconds           = var.message_retention_seconds
+  receive_wait_time_seconds           = var.receive_wait_time_seconds
+  fifo_queue                          = var.fifo_queue
+  fifo_throughput_limit               = var.fifo_queue ? var.fifo_throughput_limit : null
+  kms_master_key_id                   = var.kms_master_key_id
+  kms_data_key_reuse_period_seconds   = var.kms_data_key_reuse_period_seconds
+  deduplication_scope                 = var.fifo_queue ? var.deduplication_scope : null
+ 
+  lifecycle {
     ignore_changes = [tags]
   } 
 }
--- variables.tf
+++ variables.tf
@@ -6,21 +6,45 @@ variable "queue_name" {
   type        = list(string)
   description = "queue_name"
 }
+variable "visibility_timeout_seconds" {
+  type        = number
+  description = "The visibility timeout for the queue. An integer from 0 to 43200 (12 hours)"
+}
 variable "delay_seconds" {
   type        = number
   description = "The time in seconds that the delivery of all messages in the queue will be delayed.An integer from 0 to 900,default 0"
 }
 variable "max_message_size" {
   type        = number
-  description = "The limit of how many bytes a message can contain.An integer from 1024 bytes (1 KiB) up to 262144 bytes (256 KiB),default is 262144 (256 KiB)."
+  description = "The limit of how many bytes a message can contain before Amazon SQS rejects it. An integer from 1024 bytes (1 KiB) up to 262144 bytes (256 KiB). The default for this attribute is 262144 (256 KiB)"
 }
 variable "message_retention_seconds" {
   type        = number
-  description = "The number of seconds SQS retains a message. Integer representing seconds, from 60 (1 minute) to 1209600 (14 days),default is 345600 (4 days)"
+  description = "The number of seconds Amazon SQS retains a message. Integer representing seconds, from 60 (1 minute) to 1209600 (14 days). The default for this attribute is 345600 (4 days"
 }
 variable "receive_wait_time_seconds" {
   type        = number
   description = "The time for which a ReceiveMessage call will wait for a message to arrive (long polling) before returning. An integer from 0 to 20 (seconds), default is 0"
+}
+variable "fifo_queue" {
+  type        = bool
+  description = "Whether SQS is FIFO or not"
+}
+variable "fifo_throughput_limit" {
+  type        = string
+  description = "Specifies whether the FIFO queue throughput quota applies to the entire queue or per message group. Valid values are perQueue (default) and perMessageGroupId"
+}
+variable "kms_master_key_id" {
+  type        = string
+  description = "KMS key ARN for the SQS."
+}
+variable "kms_data_key_reuse_period_seconds" {
+  type        = number
+  description = "The length of time, in seconds, for which Amazon SQS can reuse a data key to encrypt or decrypt messages before calling AWS KMS again"
+}
+variable "deduplication_scope" {
+  type        = string
+  description = "Specifies whether message deduplication occurs at the message group or queue level"
 }
 
 variable "region" {
